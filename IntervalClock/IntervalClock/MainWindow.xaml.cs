31cc3a3ac831057a60796c43be7432b33f701785
heltonbiker <heltonbiker@gmail.com>
2018-01-30 18:50:39 -0200

    Adicionados projetos

---
+++ IntervalClock/IntervalClock/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IntervalClock
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			CompositionTarget.Rendering += CompositionTarget_Rendering;
+		}
+
+		private void CompositionTarget_Rendering(object sender, EventArgs e)
+		{
+			var viewModel = DataContext as ViewModel;
+
+			timeText.Text = viewModel?.GetTime();
+		}
+	}
+}
---
+++ IntervalClock/IntervalClock/ViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace IntervalClock
+{
+	public class ViewModel //: INotifyPropertyChanged
+	{
+
+		//public double TimeSeconds
+		//{
+		//	get { return _timeSeconds; }
+		//	set
+		//	{
+		//		_timeSeconds = value;
+		//		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TimeSeconds"));
+		//	}
+		//}
+		//double _timeSeconds;
+
+
+		//onst double MILLIS_INTERVAL = 10;
+
+		public ViewModel()
+		{
+			//Observable.Interval(TimeSpan.FromMilliseconds(MILLIS_INTERVAL))
+			//		  .Subscribe(token => Task.Run(() => TimeSeconds += MILLIS_INTERVAL / 1000));
+		}
+
+
+		//public event PropertyChangedEventHandler PropertyChanged;
+
+
+		Stopwatch _sw = Stopwatch.StartNew();
+
+
+
+		public string GetTime()
+		{
+			return (_sw.ElapsedMilliseconds / 1000.0).ToString("N3");
+		}
+	}
+}
